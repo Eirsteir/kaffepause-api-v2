3c82dc41e341b489bcec9ccc6826acd45438c528
Eirik Steira <steiraeirik@gmail.com>
2023-05-31 14:59:48 +0200

    Finish neo4j setup w/docker and app config + add black formatter

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,4 @@
-
-from typing import List, Union
+from typing import Any, Dict, List, Optional, Union
 
 from pydantic import AnyHttpUrl, BaseSettings, validator
 
@@ -16,11 +15,20 @@ class Settings(BaseSettings):
             return v
         raise ValueError(v)
 
-    NEO4J_CONNECTION_URL: str
+    NEO4J_USER: str
+    NEO4J_PASSWORD: str
+    NEO4J_HOST: str
+    NEO4J_PORT: str
+    NEO4J_CONNECTION_URL: Optional[str] = None
 
-    class Config:
-        case_sensitive = True
-        env_file = ".env"
+    @validator("NEO4J_CONNECTION_URL", pre=True)
+    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return (
+            f"neo4j+s://{values.get('NEO4J_USER')}:{values.get('NEO4J_PASSWORD')}"
+            f"@{values.get('NEO4J_HOST')}:{values.get('NEO4J_PORT')}"
+        )
 
 
 settings = Settings()
--- app/database.py
+++ app/database.py
@@ -1,11 +1,9 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
-from neomodel import config, db
+from neomodel import config
 
 from app.core.config import settings
-
-config.DATABASE_URL = settings.NEO4J_CONNECTION_URL
 
 
 @asynccontextmanager
@@ -16,6 +14,8 @@ async def lifespan(application: FastAPI):
 
 async def initialize_neomodel():
     # Initialize the Neo4j connection
+    config.DATABASE_URL = settings.NEO4J_CONNECTION_URL
+
     config.AUTO_INSTALL_LABELS = True
     config.AUTO_INSTALL_PROPERTIES = True
     config.FORCE_REFRESH_ENTITIES = True
@@ -25,9 +25,3 @@ async def initialize_neomodel():
     config.AUTO_INDEX_SCHEMAS = True
     config.USE_ARRAY_FIELD = True
     config.AUTO_SAVE_ENABLED = True
-
-    await db.set_connection(config.DATABASE_URL)
-    await db.set_connection(config.DATABASE_URL + '/' + settings.NEO4J_DATABASE_NAME)
-
-    # Verify the connection
-    db.cypher_query("MATCH (n) RETURN n LIMIT 1")
